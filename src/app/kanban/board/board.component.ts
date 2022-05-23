43ee29144321cbf72809011ceb77086e7981dd85
zettacamp-dzulfahmi-mutahhary <dzulfahmi.mutahhary@zettacamp.pro>
2022-05-23 20:30:32 +0700

    up

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, Input, OnInit } from '@angular/core';
+import { Board } from '../board.model';
+import { BoardService } from '../board.service';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  @Input() board: Board;
+  constructor(private boardService: BoardService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  taskDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
+    this.boardService.updateTask(this.board.id, this.board.tasks);
   }
-
 }
